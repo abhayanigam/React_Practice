55b93716a08019978be9b50c538e2779442cfdc4
Abhaya Nigam <abhaya.nigam13@gmail.com>
2024-07-28 05:37:49 +0530

    Added Routes

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextFrom";
 import Alert from "./components/Alert";
+import About from "./components/About";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState("light"); // Whether dark mode Enabled or not
@@ -31,23 +34,41 @@ function App() {
       document.title = "TextUtils - Light Mode";
     }
   };
+
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        about="About"
-        home="Home"
-        mode={darkMode}
-        toggleMode={toggleMode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter The Text To Analyz Below"
+      <Router>
+        <Navbar
+          title="TextUtils"
+          about="About"
+          home="Home"
           mode={darkMode}
+          toggleMode={toggleMode}
         />
-      </div>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            {/*
+            With the use exact it matches completly otherwise it matches
+            partially 
+              /users --> Component 1
+              /users/home --> Component 2 
+            */}
+            <Route exact path="/about" element={<About mode={darkMode} />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter The Text To Analyze Below"
+                  mode={darkMode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
